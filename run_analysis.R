2fb2a90d5d7d379fbbe2d53447ab21952e39a4ea
thehai <nthehai01@gmail.com>
2022-02-09 14:51:45 +0700

    update code for part 1 and 2 for run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -9,23 +9,39 @@ features %<>% strsplit(" ") %>%
 
 ## Load data
 X_train <- read.table('./data/train/X_train.txt', col.names = features)
-y_train <- read.table('./data/train/y_train.txt')
+y_train <- read.table('./data/train/y_train.txt', col.names = "activity")
+subject_train <- read.table("./data/train/subject_train.txt", col.names = "subject")
+
 X_test <- read.table('./data/test/X_test.txt', col.names = features)
-y_test <- read.table('./data/test/y_test.txt')
+y_test <- read.table('./data/test/y_test.txt', col.names = "activity")
+subject_test <- read.table("./data/test/subject_test.txt", col.names = "subject")
 
 
 ## 1. Merges the training and the test sets to create one data set.
-## Add activity column
-X_train$activity <- y_train
-X_test$activity <- y_test
+## Add subject and activity columns
+X_train <- cbind(y_train, subject_train, X_train)
+X_test <- cbind(y_test, subject_test, X_test)
 
 ## Merge datasets
-tidyDataset <- rbind(X_train, X_test)
+mergedDataset <- rbind(X_train, X_test)
 
 
-## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-meanMesurement <- lapply(select(tidyDataset, -activity), mean)
-stdMesurement <- lapply(select(tidyDataset, -activity), sd)
+## 2. Extracts only the measurements on the mean 
+## and standard deviation for each measurement. 
+## Extract the indexes of proper features in 'features' vector
+properIndexes <- grep("(?:mean|std)", features)
+
+## Due to the first and second columns in the 'mergedDataset' data frame
+## is 'activity' and 'subject', to convert the 'properIndexes' of 'features' vector
+## to the indexes in 'mergedDataset', we need to add 2 to each index.
+properIndexes.mergedDataset <- properIndexes + 2
+
+## Add indexes of 'activity' and 'subject' columns to 
+## the proper column indexes in 'mergedDataset'
+properIndexes.mergedDataset <- append(c(1, 2), properIndexes.mergedDataset)
+
+## Extract features 
+df <- mergedDataset[, properIndexes.mergedDataset]
 
 
 ## 3. Uses descriptive activity names to name the activities in the data set
@@ -35,13 +51,15 @@ activity_labels %<>% strsplit(" ") %>%
 
 
 ## 4. Appropriately labels the data set with descriptive variable names. 
-tidyDataset$activity <- sapply(tidyDataset$activity, function(ele) activity_labels[ele])
+df$activity <- sapply(df$activity, 
+                      function(ele) activity_labels[ele])
 
 
 ## 5. From the data set in step 4, creates a second, 
-##    independent tidy data set with the average of each variable for each activity and each subject.
-grouped <- tidyDataset %>% 
-    group_by(activity) %>%
+##  independent tidy data set with the average of each variable 
+##  for each activity and each subject.
+tidyDataset <- df %>% 
+    group_by(subject, activity) %>%
     summarise_all(mean)
 
 
